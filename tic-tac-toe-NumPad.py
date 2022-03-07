6778d4feff9aa022584a53cd622b812c27a5b92b
october-june <a.pu6yc@gmail.com>
2022-03-08 00:26:51 +0300

    Added game scores

--- tic-tac-toe-NumPad.py
+++ tic-tac-toe-NumPad.py
@@ -14,22 +14,22 @@ WIN_CONDITIONS = (
 
 # NumPad coordinates
 MOVES = {
-    7: (1, 0), 8: (1, 1), 9: (1, 2),
-    4: (2, 0), 5: (2, 1), 6: (2, 2),
-    1: (3, 0), 2: (3, 1), 3: (3, 2)
+    7: (0, 0), 8: (0, 1), 9: (0, 2),
+    4: (1, 0), 5: (1, 1), 6: (1, 2),
+    1: (2, 0), 2: (2, 1), 3: (2, 2)
 }
 
-class TEXT:
+class txt:
     bold = '\033[1m'
     yellow = '\033[43m'
     inverse = '\033[37m\033[40m'
     end = '\033[0;0m'
 
-    def make_frame(self):
-        return TEXT.bold + TEXT.inverse + self.center(19) + TEXT.end
+    def make_title(self):
+        return txt.bold + txt.inverse + self.center(19) + txt.end
 
     def winner(self):
-        return TEXT.yellow + self.center(5) + TEXT.end
+        return txt.yellow + self.center(5) + txt.end
 
 
 # Cleans the console
@@ -42,7 +42,7 @@ def choose_char():
     while True:
         try:
             human = input('Сыграть за "x" или "o"?\n').casefold()
-            assert human in ('x', 'х', 'o', 'о')
+            assert human in ('x', 'х', 'o', 'о')  # cyrillic or latin
         except AssertionError:
             print('Ошибка ввода.')
         else:
@@ -57,32 +57,33 @@ def get_answer(question):
     return answer in ('да', 'lf', 'yes')
 
 
-# Create board with title
-def create_field():
-    field = [[' '] * 3 for i in range(4)]
-    field[0] = TEXT.make_frame('Tic-Tac-Toe')
-    return field
-
+# Create dict for game score
+# Depend on game type: vs human or vs AI
+def create_score_table(P1, P2):
+    scores = {'draw:': 0, 'round:': 1}
+    scores[(f'{P1}:', 'win:')[bool(ai)]] = 0
+    scores[(f'{P2}:', 'lose:')[bool(ai)]] = 0
+    return scores
 
 
 # Print field with rows and columns
 def print_field():
-    # Title
-    divider = '┌─────┬─────┬─────┐'
-    print(f'{field[0]}\n{divider}')
+    # Title with scores
+    print(txt.make_title('round: ' + str(game_score['round:'])))
+    score_line = ' '.join(key + str(value) for key, value in game_score.items() if key != 'round:')
+    print(txt.make_title(score_line))
 
     # Body
-    for row in field[1:]:
+    print('┌─────┬─────┬─────┐')
+
+    for row in field:
         print('', end='│')
         for c in row:
             print(f'{c.center(5)}', end='│')
+        if row is not field[-1]:
+            print('\n├─────┼─────┼─────┤')
 
-        if row is field[-1]:
-            divider = '└─────┴─────┴─────┘'
-        else:
-            divider = '├─────┼─────┼─────┤'
-
-        print('\n' + divider)
+    print('\n└─────┴─────┴─────┘')
 
 
 # Check the input is correct
@@ -91,7 +92,7 @@ def get_coord():
         try:
             move = int(input('Введите координаты:\n'))
             assert 1 <= move <= 9
-        except (AssertionError, ValueError):
+        except (AssertionError, ValueError, KeyError):
             print('Неверные координаты!')
         else:
             return move
@@ -111,21 +112,27 @@ def check_win_cond(char):
             # Highlights winner's row
             for coord in condition:
                 x, y = MOVES[coord]
-                field[x][y] = TEXT.winner(char.center(3))
+                field[x][y] = txt.winner(char.center(3))
             return True
 
     return False
 
 
 # Primitive AI makes move
-def ai_turn(char1, char2):
-    # Move to win or save
+def ai_turn(ai_char, human_char):
+    # Move to win (primarily) or save
+    cell = None
     for condition in WIN_CONDITIONS:
-        if sum(cell in PLAYERS[char1] for cell in condition) == 2 \
-        or sum(cell in PLAYERS[char2] for cell in condition) == 2:
-            for cell in condition:
-                if valid_move(cell):
-                    return cell
+        if sum(point in PLAYERS[ai_char] for point in condition) == 2:
+            for c in condition:
+                if valid_move(c):
+                    cell = c
+        elif sum(point in PLAYERS[human_char] for point in condition) == 2:
+            for c in condition:
+                if not cell and valid_move(c):
+                    cell = c
+    if cell:
+        return cell
     # Random move
     else:
         cell = 5
@@ -165,16 +172,26 @@ def play():
                     sleep(1)
                 break
 
-        # End with win or tie
+        # End with win or draw
         if turn >= 5:
             winner = check_win_cond(char)
         if turn == 9:
             break
 
+    # Scores update
+    if winner:
+        if ai:
+            game_score[('win:', 'lose:')[ai == char]] += 1
+        else:
+            game_score[(f'{player1}:', f'{player2}:')[char == 'o']] += 1
+    else:
+        game_score['draw:'] += 1
+
     # Congrats
     clean()
     print_field()
     print(('Ничья!\n', f'Победил "{char}"!\n')[winner])
+    game_score['round:'] += 1
     sleep(1)
 
 
@@ -184,7 +201,8 @@ sleep(0.5)
 print('''
     Инструкция:
 Вы делаете ходы по очереди. Начинает "x".
-Чтобы сделать ход, используйте числа на NumPad от 1 до 9.\n
+Чтобы сделать ход, используйте числа на NumPad от 1 до 9.
+И разверните консоль пошире ;)\n
     Хорошей игры!\n''')
 sleep(0.5)
 
@@ -193,13 +211,18 @@ if get_answer('Введите "да", если хотите сыграть с к
     human, ai = choose_char()
 else:
     ai = False
+    player1 = input('Введите имя первого игрока:\n')[:3]
+    player2 = input('Введите имя второго игрока:\n')[:3]
 
-# Reset moves, field and play again
+# Keep statistics
+game_score = create_score_table(player1, player2)
+
+# Start, reset moves, field and play again
 answer = True
 while answer:
     PLAYERS['o'].clear()
     PLAYERS['x'].clear()
-    field = create_field()
+    field = [[' '] * 3 for _ in range(3)]
     play()
     print('Сыграете ещё раз?')
     answer = get_answer('Введите "да", если хотите сыграть снова')
